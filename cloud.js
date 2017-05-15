702837c2e2d35d98d8e758d952b0609332108f00
fengjian <fengjian@lez.cn>
2017-05-15 16:10:49 +0800

    feat(getPlayUrl): support get play url interface

--- cloud.js
+++ cloud.js
@@ -12,3 +12,9 @@ AV.Cloud.define('baidu', function(request) {
     let res = req('GET', 'http://www.baidu.com')
     return res.getBody('utf8')
 })
+AV.Cloud.define('getPlayUrl', function(request) {
+
+    let url = request.params.url
+    let res = req('GET', 'http://jxs.s.yunfan.com/remote/parse_kuaibo_new.php?format=high&ext=mp4&url=' + url)
+    return res.getBody('utf8')
+})
